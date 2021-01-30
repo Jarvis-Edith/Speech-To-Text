d6a4ae611d6453bcb2e58ea824ce5217f449e8cf
JarvisEdith <jarvispeculiardata@gmail.com>
2021-01-31 02:42:09 +0530

    updated-code

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-var speechRecognition = window.webkitSpeechRecognitio;
+var speechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
 
-var recognition = new speechRecognition();
+var recognition = new speechRecognition()
 
-var textbox = $("#textbox");
+var textbox = $("#textbox")
 
-var instructions = $("#instructions");
+var instructions = $("#instructions")
 
-var content = '';
-recognition.continuous = true;
+var content = ''
+recognition.continuous = true
 
 // recognition is started
 recognition.onstart = function () {
-    instructions.text("Voice Recognition is on");
+    instructions.text("Voice Recognition is on")
 }
 
 recognition.onspeechend = function () {
-    instructions.text("No Activity");
+    instructions.text("Voice Recognition is off")
 }
 
 recognition.onerror = function () {
-    instructions.text("Try again..!");
+    instructions.text("Try again..!")
 }
 
 recognition.onresult = function (event) {
-    let current = event.resultIndex;
-    let transcript = event.results[current][0].transcript;
-    content += transcript;
-    textbox.val(content);
+    let current = event.resultIndex
+    let transcript = event.results[current][0].transcript
+    content += transcript
+    textbox.val(content)
 }
 
 $("#start-btn").click(function (event) {
     if (content.length) {
-        content += '';
+        content += ''
     }
-    recognition.start();
+    recognition.start()
+})
+
+textbox.on('input', function () {
+    content = $(this).val()
+})
+
+$("#stop-btn").click(function (event) {
+    recognition.stop()
 })
